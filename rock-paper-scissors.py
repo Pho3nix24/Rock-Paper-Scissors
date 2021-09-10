810da7f53626ef31ab3c4732fb7edb138a54e5bf
Aditya A <sra55tvm@gmail.com>
2021-09-10 19:08:02 +0530

    Version 1.2

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,38 +1,35 @@
 import random
 
-options = ["rock", "paper", "scissors"]
+winning_options = {1: (2, 1, "Player"), 2: (3, 2, "Player"), 3: (1, 3, "Player"), 4: (1, 2, "CPU"), 5: (2, 3, "CPU"),
+                   6: (3, 1, "CPU"), 7: (1, 1, "Tie"), 8: (2, 2, "Tie"), 9: (3, 3, "Tie")}
+options = {"rock": 1, "paper": 2, "scissors": 3}
+scores = {"Player": 0, "CPU": 0, "Tie": 0}
 play_again = "yes"
+round_winner = None
+history = {1: []}
 
-while play_again == "yes":
-    history = {1: []}
-    user_points = 0
-    cpu_points = 0
-    no_of_rounds = int(input("Enter the number of Rounds : "))
-    for i in range(0, no_of_rounds):
-        user_input = input("Player choice (rock, paper, scissors): ")
-        cpu_input = random.choice(options)
-        print("CPU choice : {}".format(cpu_input))
-        if user_input == "rock" and cpu_input == "scissors" or user_input == "scissors" and cpu_input == "paper" or user_input == "paper" and cpu_input == "rock":
-            round_winner = "Player"
-            user_points = user_points + 1
-        elif user_input == "scissors" and cpu_input == "rock" or user_input == "paper" and cpu_input == "scissors" or user_input == "rock" and cpu_input == "paper":
-            round_winner = "CPU"
-            cpu_points = cpu_points + 1
-        else:
-            round_winner = "Tie"
-        history[i + 1] = [user_input, cpu_input, round_winner]
-    print("Player Score : ", user_points)
-    print("CPU Score    : ", cpu_points)
-    if user_points > cpu_points:
-        print("Winner of the series is Player")
-    elif user_points < cpu_points:
-        print("Winner of the series is CPU")
-    else:
-        print("The Series is Tied")
+no_of_rounds = int(input("Enter the number of Rounds : "))
+for i in range(0, no_of_rounds):
+    user_input = user_choice = (input("Player choice (rock, paper, scissors): "))
+    user_input = options[user_input]
+    cpu_input = cpu_choice = random.choice(list(options.keys()))
+    print("CPU choice : {}".format(cpu_input))
+    cpu_input = options[cpu_input]
+    for j in winning_options:
+        if winning_options[j][0] == user_input and winning_options[j][1] == cpu_input:
+            round_winner = winning_options[j][2]
+            scores[round_winner] = scores[round_winner] + 1
+        history[i + 1] = [user_choice, cpu_choice, round_winner]
+print("Player Score : {}\nCPU Score    : {}".format(scores["Player"], scores["CPU"]))
+if scores["Player"] > scores["CPU"]:
+    print("Winner of the series is Player")
+elif scores["Player"] < scores["CPU"]:
+    print("Winner of the series is CPU")
+else:
+    print("The Series is Tied")
+round_history = int(input("Enter the round whose information is needed (0 to exit): "))
+while round_history != 0:
+    print("Player Choice : {}\nCPU Choice    : {}\nRound Winner  : {}".format(history[round_history][0],
+                                                                              history[round_history][1],
+                                                                              history[round_history][2]))
     round_history = int(input("Enter the round whose information is needed (0 to exit): "))
-    while round_history != 0:
-        print("Player Choice : {}\nCPU Choice    : {}\nRound Winner  : {}".format(history[round_history][0],
-                                                                                  history[round_history][1],
-                                                                                  history[round_history][2]))
-        round_history = int(input("Enter the round whose information is needed (0 to exit): "))
-    play_again = input("Do you wish to play again? (yes/no) : ")
